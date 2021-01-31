a1433c863c7e613932ec96ad1573e633b8e6465b
Larry Shields <larry.shields@gmail.com>
2021-01-31 12:53:39 -0500

    Added Team Transformer with Tests

---
+++ src/NbaStats.Loader.Tests/Transformers/TeamTransformerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NbaStats.Data.DataObjects;
+using NbaStats.Loader.DataObject;
+using NbaStats.Loader.Transformers;
+
+namespace NbaStats.Loader.Tests.Transformers
+{
+    [TestFixture]
+    public class TeamTransformerTests
+    {
+
+        private TeamTransformer transformer;
+        private TeamEntry entry;
+
+        [SetUp]
+        public void Setup()
+        {
+            transformer = new TeamTransformer();
+
+            entry = new TeamEntry()
+            {
+                Code = "IND",
+                Type = "home",
+                Name = "Indiana Pacers"
+            };
+            entry.AddLineScore(new LineScore(1, 20));
+            entry.AddLineScore(new LineScore(2, 15));
+            entry.AddLineScore(new LineScore(3, 25));
+            entry.AddLineScore(new LineScore(4, 30));
+            entry.AddLineScore(new LineScore(5, 14));
+            entry.AddLineScore(new LineScore(6, 16));
+        }
+
+        [Test]
+        public void TestTransformTeam()
+        {
+            var team = transformer.Transform(entry);
+            Assert.AreEqual("IND", team.TeamCode);
+            Assert.AreEqual("Indiana Pacers", team.TeamName);
+        }
+
+        [Test]
+        public void TestTransformBoxScore()
+        {
+            var boxscore = transformer.TransformBoxScore(entry);
+            Assert.AreEqual(20, boxscore.Quarter1);
+            Assert.AreEqual(15, boxscore.Quarter2);
+            Assert.AreEqual(25, boxscore.Quarter3);
+            Assert.AreEqual(30, boxscore.Quarter4);
+            Assert.AreEqual(30, boxscore.Ot);
+            Assert.AreEqual(120, boxscore.Total);
+
+        }
+
+    }
+}
---
+++ src/NbaStats.Loader/Transformers/TeamTransformer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NbaStats.Data.DataObjects;
+using NbaStats.Loader.DataObject;
+using NbaStats.Loader.Constants;
+using NbaStats.Loader.Helpers;
+using System.Linq;
+
+namespace NbaStats.Loader.Transformers
+{
+    public class TeamTransformer
+    {
+        public Team Transform(TeamEntry entry)
+        {
+            return new Team()
+            {
+                TeamCode = entry.Code,
+                TeamName = entry.Name
+            };
+        }
+
+        public BoxScoreEntry TransformBoxScore(TeamEntry entry)
+        {
+            var boxscore = new BoxScoreEntry()
+            {
+                Quarter1 = GetLineScoreValue(entry.LineScores, 1),
+                Quarter2 = GetLineScoreValue(entry.LineScores, 2),
+                Quarter3 = GetLineScoreValue(entry.LineScores, 3),
+                Quarter4 = GetLineScoreValue(entry.LineScores, 4)
+            };
+
+            boxscore.Ot = entry.LineScores.Where(c => c.Quarter > 4).Sum(c => c.Score);
+            boxscore.Total = boxscore.Quarter1 + boxscore.Quarter2 + boxscore.Quarter3 + boxscore.Quarter4 + boxscore.Ot;
+            return boxscore;            
+        }
+
+        private int GetLineScoreValue(List<LineScore> scores, int quarter)
+        {
+            var lineScore = scores.Where(c => c.Quarter == quarter).FirstOrDefault();
+            if (lineScore != null)
+            {
+                return lineScore.Score;
+            }
+            return 0;
+
+        }
+
+    }
+}
