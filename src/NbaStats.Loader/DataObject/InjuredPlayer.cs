6aaf9b6e88a4288b0183dd3393439778bb354011
Larry Shields <larry.shields@gmail.com>
2021-02-06 09:37:06 -0500

    Added Injury Json Objects

---
+++ src/NbaStats.Loader/DataObject/InjuredPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace NbaStats.Loader.DataObject
+{
+    public class InjuredPlayer
+    {
+
+        [JsonProperty(PropertyName = "playerName")]
+        public string PlayerName { get; set; }
+
+        [JsonProperty(PropertyName = "date")]
+        public string Date { get; set; }
+
+        [JsonProperty(PropertyName = "position")]
+        public string Position { get; set; }
+
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+    }
+}
---
+++ src/NbaStats.Loader/DataObject/InjuryEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace NbaStats.Loader.DataObject
+{
+    public class InjuryEntry
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string TeamName { get; set; }
+
+        [JsonProperty(PropertyName = "injuries")]
+        public List<InjuredPlayer> Players { get; set; }
+
+        public InjuryEntry()
+        {
+            Players = new List<InjuredPlayer>();
+        }
+
+    }
+}
