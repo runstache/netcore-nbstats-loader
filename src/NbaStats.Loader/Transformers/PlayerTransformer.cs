c6e78fcddc197f862f0e5a7ea165be467a8aec91
Larry Shields <larry.shields@gmail.com>
2021-02-06 09:36:43 -0500

    Updated Stat for Non Playing Entry

--- src/NbaStats.Loader/Transformers/PlayerTransformer.cs
+++ src/NbaStats.Loader/Transformers/PlayerTransformer.cs
@@ -41,91 +41,95 @@ namespace NbaStats.Loader.Transformers
         /// <returns>Player Stat</returns>
         public PlayerStat TransformStat(PlayerEntry entry)
         {
-            var stat = new PlayerStat()
+            if (!entry.Stats.Any(s => s.Name.ToLower() == "dnp"))
             {
-                Assists = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Assists)),
-                Blocks = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Blocks)),
-                DefensiveRebound = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.DefensiveRebounds)),
-                Fouls = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Fouls)),
-                GameMinutes = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Minutes)),
-                OffensiveRebound = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.OffensiveRebounds)),
-                Points = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Points)),
-                Steals = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Steals)),
-                Turnovers = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.TurnOvers))
-            };
-
-            // Three Point Statistics
-            string threePoint = GetStatValue(entry.Stats, PlayerStatConstants.ThreePoint);
-            if (!string.IsNullOrEmpty(threePoint))
-            {
-                string[] parts = threePoint.Split("-");
-                if (parts.Length > 1)
+                var stat = new PlayerStat()
                 {
-                    stat.ThreeCompleted = DataTypeHelper.ConvertToInteger(parts[0]);
-                    stat.ThreeTaken = DataTypeHelper.ConvertToInteger(parts[1]);
-                    if (stat.ThreeCompleted > 0 && stat.ThreeTaken > 0)
+                    Assists = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Assists)),
+                    Blocks = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Blocks)),
+                    DefensiveRebound = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.DefensiveRebounds)),
+                    Fouls = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Fouls)),
+                    GameMinutes = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Minutes)),
+                    OffensiveRebound = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.OffensiveRebounds)),
+                    Points = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Points)),
+                    Steals = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.Steals)),
+                    Turnovers = DataTypeHelper.ConvertToInteger(GetStatValue(entry.Stats, PlayerStatConstants.TurnOvers))
+                };
+
+                // Three Point Statistics
+                string threePoint = GetStatValue(entry.Stats, PlayerStatConstants.ThreePoint);
+                if (!string.IsNullOrEmpty(threePoint))
+                {
+                    string[] parts = threePoint.Split("-");
+                    if (parts.Length > 1)
                     {
-                        double pct = Convert.ToDouble(stat.ThreeCompleted) / Convert.ToDouble(stat.ThreeTaken);
-                        stat.ThreePercentage = Math.Round(pct, 2);
-                    }
-                    else
-                    {
-                        stat.ThreePercentage = 0;
+                        stat.ThreeCompleted = DataTypeHelper.ConvertToInteger(parts[0]);
+                        stat.ThreeTaken = DataTypeHelper.ConvertToInteger(parts[1]);
+                        if (stat.ThreeCompleted > 0 && stat.ThreeTaken > 0)
+                        {
+                            double pct = Convert.ToDouble(stat.ThreeCompleted) / Convert.ToDouble(stat.ThreeTaken);
+                            stat.ThreePercentage = Math.Round(pct, 2);
+                        }
+                        else
+                        {
+                            stat.ThreePercentage = 0;
+                        }
                     }
                 }
-            }
 
-            // Point Differential
-            string pointDiff = GetStatValue(entry.Stats, PlayerStatConstants.PointDifferential);
-            if (!string.IsNullOrEmpty(pointDiff))
-            {
-                stat.PointDifferential = DataTypeHelper.ConvertToInteger(pointDiff.Replace("+", ""));
-            }
-
-            // Free Throws Percentage
-            string foulShout = GetStatValue(entry.Stats, PlayerStatConstants.FreeThrows);
-            if (!string.IsNullOrEmpty(foulShout))
-            {
-                string[] parts = foulShout.Split("-");
-                if (parts.Length > 1)
+                // Point Differential
+                string pointDiff = GetStatValue(entry.Stats, PlayerStatConstants.PointDifferential);
+                if (!string.IsNullOrEmpty(pointDiff))
                 {
-                    stat.FTTaken = DataTypeHelper.ConvertToInteger(parts[0]);
-                    stat.FTCompleted = DataTypeHelper.ConvertToInteger(parts[1]);
+                    stat.PointDifferential = DataTypeHelper.ConvertToInteger(pointDiff.Replace("+", ""));
+                }
 
-                    if (stat.FTCompleted > 0 && stat.FTTaken > 0)
+                // Free Throws Percentage
+                string foulShout = GetStatValue(entry.Stats, PlayerStatConstants.FreeThrows);
+                if (!string.IsNullOrEmpty(foulShout))
+                {
+                    string[] parts = foulShout.Split("-");
+                    if (parts.Length > 1)
                     {
-                        double pct = Convert.ToDouble(stat.FTCompleted) / Convert.ToDouble(stat.FTTaken);
-                        stat.FTPercentage = Math.Round(pct, 2);
-                    }
-                    else
-                    {
-                        stat.FTPercentage = 0;
+                        stat.FTTaken = DataTypeHelper.ConvertToInteger(parts[0]);
+                        stat.FTCompleted = DataTypeHelper.ConvertToInteger(parts[1]);
+
+                        if (stat.FTCompleted > 0 && stat.FTTaken > 0)
+                        {
+                            double pct = Convert.ToDouble(stat.FTCompleted) / Convert.ToDouble(stat.FTTaken);
+                            stat.FTPercentage = Math.Round(pct, 2);
+                        }
+                        else
+                        {
+                            stat.FTPercentage = 0;
+                        }
                     }
                 }
-            }
 
-            // Field Goal Percentage
-            string fieldGoals = GetStatValue(entry.Stats, PlayerStatConstants.FieldGoals);
-            if (!string.IsNullOrEmpty(fieldGoals))
-            {
-                string[] parts = fieldGoals.Split("-");
-                if (parts.Length > 1)
+                // Field Goal Percentage
+                string fieldGoals = GetStatValue(entry.Stats, PlayerStatConstants.FieldGoals);
+                if (!string.IsNullOrEmpty(fieldGoals))
                 {
-                    stat.FGTaken = DataTypeHelper.ConvertToInteger(parts[1]);
-                    stat.FGCompleted = DataTypeHelper.ConvertToInteger(parts[0]);
+                    string[] parts = fieldGoals.Split("-");
+                    if (parts.Length > 1)
+                    {
+                        stat.FGTaken = DataTypeHelper.ConvertToInteger(parts[1]);
+                        stat.FGCompleted = DataTypeHelper.ConvertToInteger(parts[0]);
 
-                    if (stat.FGCompleted > 0 && stat.FGTaken > 0)
-                    {
-                        double pct = Convert.ToDouble(stat.FGCompleted) / Convert.ToDouble(stat.FGTaken);
-                        stat.FGPercentage = Math.Round(pct, 2);
-                    }
-                    else
-                    {
-                        stat.FGPercentage = 0;
+                        if (stat.FGCompleted > 0 && stat.FGTaken > 0)
+                        {
+                            double pct = Convert.ToDouble(stat.FGCompleted) / Convert.ToDouble(stat.FGTaken);
+                            stat.FGPercentage = Math.Round(pct, 2);
+                        }
+                        else
+                        {
+                            stat.FGPercentage = 0;
+                        }
                     }
                 }
-            }                      
-            return stat;
+                return stat;
+            }
+            return null;
         }
 
         private string GetStatValue(List<StatEntry> stats, string key)
